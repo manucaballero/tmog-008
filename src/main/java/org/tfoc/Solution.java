5c6a243c78ede893dcf1bbf95956a9bce251356a
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-03-07 08:53:21 +0100

    Solved galadriel 008

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,36 @@
 package org.tfoc;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public Integer search(
+    public static Integer search(
             Integer[] nums,
             Integer target) {
 
-        return null;
+        boolean isFound = false;
+        int arraySize = nums.length;
+        while(!isFound && arraySize >= 1){
+
+            int halfNumberPos = nums.length / 2;
+            if(Objects.equals(nums[halfNumberPos], target)){
+
+                isFound = true;
+            }else if(nums[halfNumberPos] > target){
+
+                nums = Arrays.copyOfRange(nums, 0, halfNumberPos);
+            }else if (nums[halfNumberPos] < target){
+
+                nums = Arrays.copyOfRange(nums, halfNumberPos, nums.length);
+            }
+            arraySize = halfNumberPos;
+        }
+
+        return isFound ? target : -1;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,41 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void testSolution01() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        Integer result = Solution.search(new Integer[]{0,1}, 0);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testSolution02() {
+
+        Integer result = Solution.search(new Integer[]{0,1,2,3,4,5,6}, 7);
+
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void testSolutionExample01() {
+
+        Integer result = Solution.search(new Integer[]{-1,0,3,5,9,12}, 9);
+
+        assertEquals(9, result);
+    }
+
+    @Test
+    void testSolutionExample02() {
+
+        Integer result = Solution.search(new Integer[]{-1,0,3,5,9,12}, 2);
+
+        assertEquals(-1, result);
     }
 
 }
